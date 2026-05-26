e3a613de419f8e19f357e404427b74b443f663e5
nshan651 <nshan651@proton.me>
2026-05-25 20:01:12 -0500

    Remove larbs refs.

--- config.h
+++ config.h
@@ -310,7 +310,6 @@ static Key keys[] = {
 
     /*
         { MODKEY,			XK_F1,		spawn,
-       SHCMD("groff -mom /usr/local/share/dwm/larbs.mom -Tpdf | zathura -") },
         { MODKEY,			XK_F2,		spawn,		{.v =
        (const char*[]){ "tutorialvids", NULL } } }, { MODKEY,
        XK_F3,		spawn,		{.v = (const char*[]){ "displayselect",
